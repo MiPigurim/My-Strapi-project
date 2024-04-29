c0295712be58cfe0b52e59f3d4ec2ef3b882368f
MiPigurim <michelepigurim26@gmail.com>
2024-04-29 12:07:32 -0300

    front-end repository

---
+++ front-end/.eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+      // inserido pelo comentário aula 45
+      requireConfigFile: false,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+      // inserido 'jsx' pelo comentário aula 45
+  plugins: ['react', 'prettier', 'react-hooks', 'jsx'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+};
